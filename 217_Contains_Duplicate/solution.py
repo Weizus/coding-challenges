8e834f0aed8f279d586f40c9502218f59ae7b574
Weizus <marcley.colin@aol.com>
2026-02-22 15:00:12 -0500

    Added next problem to solve, later

--- 217_Contains_Duplicate/solution.py
+++ 217_Contains_Duplicate/solution.py
@@ -1,18 +1,21 @@
-""" The idea is to use a hash table. Check for collisions
-    If there is a collision return true, else return false
+"""The idea is to use a hash table. Check for collisions
+If there is a collision return true, else return false
 """
+
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         hash = dict()
         for num in nums:
-            if (num in hash):
+            if num in hash:
                 return True
             hash[num] = None
         return False
+
 
 """ The idea here is to use a set
     If the number of elements in the set is equal to the number of elements in the List
     Then we return false
 """
-    def containsDuplicateSet(self, nums: List[int]) -> bool:
-        return len(set(nums)) != len(nums)
+# def containsDuplicateSet(self, nums: List[int]) -> bool:
+#     return len(set(nums)) != len(nums)
